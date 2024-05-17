60b9c7f5f2dae5368f2ba2e82bb10593cd535f87
Yeonsu1027 <rito1205@naver.com>
2024-05-17 15:47:45 +0900

    openai

--- openAI-001-UML/src/app/api/auth/[...nextauth]/route.js
+++ openAI-001-UML/src/app/api/auth/[...nextauth]/route.js
@@ -1,11 +1,10 @@
 import NextAuth from "next-auth/next";
-import { CredentialsProvider } from "next-auth/providers/credentials";
-import Email from "next-auth/providers/email";
+import CredentialsProvider from "next-auth/providers/credentials";
 
-const handler = NextAuth({
+const hander = NextAuth({
   pages: {
     signIn: "/login",
-    signOut: "/logout",
+    singOut: "/logout",
     error: "/auth/error",
     verifyRequest: "/auth-verify-request",
     newUser: "/join",
@@ -13,23 +12,23 @@ const handler = NextAuth({
   providers: [
     CredentialsProvider({
       name: "Email and Password",
-      Credentials: {
+      credentials: {
         email: {
           label: "이메일",
           type: "email",
-          placeholdrt: "Email 을 입력하세요",
+          placeholder: "Email 을 입력하세요",
         },
         password: {
           label: "비밀번호",
           type: "password",
-          placeholdrt: "비밀번호 를 입력하세요",
+          placeholder: "비밀번호를 입력하세요",
         },
       },
       async authorize(_, req) {
         const user = {
           id: "1",
-          name: "yeonsu",
-          email: "rito1205@naver.com",
+          name: "callor",
+          email: "callor@callor.com",
           password: "12345",
         };
         if (user) return user;
@@ -39,4 +38,4 @@ const handler = NextAuth({
   ],
 });
 
-export { handler as GET, handler as POST }; // 핸들러를 누군가가 get이나 post 로 요청하면
+export { hander as GET, hander as POST }; // 핸들러를 누군가가 get이나 post 로 요청하면
